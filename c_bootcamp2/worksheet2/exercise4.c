601a70cb6911ae9312201c6aa950bb5732e5e39b
larmitage5 <146672196+larmitage5@users.noreply.github.com>
2023-10-17 16:46:05 +0000

    Add exercises

--- c_bootcamp2/worksheet2/exercise4.c
+++ c_bootcamp2/worksheet2/exercise4.c
@@ -2,5 +2,9 @@
 int main() {
     int array[]={2,4,5,3,1};
     int i;
-    for (i=1;i++;i<4)
+
+    for (i=0;i++;i<4)
+    {
+        printf("%d", array[i+1]);
+    }
 }
---
+++ c_bootcamp2/worksheet2/exercise5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int array[]={6,3,2,6,4,7};
+    int a;
+
+    for (a=0;a++;a<6)
+    if (array[0]=array[a])
+    {
+ 
+    }
+}
