00c574a33b82ad3ab78714c7783b5b226348b43a
larmitage5 <146672196+larmitage5@users.noreply.github.com>
2023-10-18 13:44:36 +0000

    Add exercises

--- c_bootcamp2/worksheet2/exercise5.c
+++ c_bootcamp2/worksheet2/exercise5.c
@@ -2,10 +2,13 @@
 int main() {
     int array[]={6,3,2,6,4,7};
     int a;
+    int duplicate;
 
-    for (a=0;a++;a<6)
+    for (a=0;a++;a<6){
     if (array[0]=array[a])
     {
- 
+        duplicate=array[a];
+        printf("%d", duplicate);
+    }
     }
 }
